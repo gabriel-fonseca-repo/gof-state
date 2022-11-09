0fa3a8e5ff7c17b49e47aa615c5e54e4e9d46efe
gabriel.fonseca <gabriel.fonseca@hmit.com.br>
2022-11-09 14:07:13 -0300

    Commit inicial

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,6 @@ public class Main {
 		pessoaEmCadastro.getState().irAoProximoPasso();
 
 		pessoaEmCadastro.getState().executarPassoCadastro();
-		pessoaEmCadastro.getState().irAoProximoPasso();
 
 	}
 }
